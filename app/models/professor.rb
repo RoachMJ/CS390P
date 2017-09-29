8f7457618a19d7a7564f6d0a29e6247d76b71c0e
Roach <climbingroach@gmail.com>
2017-09-28 23:14:30 -0600

    Validations added for forms

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,8 @@
 class Professor < ApplicationRecord
-  has_many :sections
+  has_many :sections, :dependent => :destroy
+  has_many :courses, through: :sections
+
+  validates :professors_names, uniqueness: true
+  validates :professors_names, :length => { :in => 2..20}
+  validates :professors_names, format: { with: /\A[a-zA-Z\s]+\z/,message: "no numbers allowed, only letters"}
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,7 @@
 class Section < ApplicationRecord
   belongs_to :professor
   belongs_to :course
+
+  validates :number, numericality: { only_integer: true }
+  validates :number, length: { is: 4 }
 end
