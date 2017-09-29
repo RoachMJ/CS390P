2ddb28bbd99f9d43024a8a38b93e1c272c0daef6
Roach <climbingroach@gmail.com>
2017-09-28 23:08:50 -0600

    Search implemented in controllers

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -5,6 +5,8 @@ class ProfessorsController < ApplicationController
   # GET /professors.json
   def index
     @professors = Professor.all
+    @professors = Professor.where('number LIKE ?', "%#{params[:q]}%").order('id DESC')
+
   end
 
   # GET /professors/1
